430fde764bb294b42185324e3619ecc87db931eb
fx <15608220393@163.com>
2018-05-21 14:57:05 +0800

    update521

---
+++ lte-eNB/enb_ue/test/lte-rece.cc
@@ -0,0 +1,64 @@
+#include "FuncHead.h"
+
+#define PAYLOAD_SIZE 400
+
+using namespace srslte;
+using namespace srsue;
+ 
+extern demux mac_demux_test;
+extern mac_dummy_timers timers_test;
+/**************************************************************************
+* ipsend:从tun中读数据并压入队列
+**************************************************************************/
+
+void* lte_rece(void *ptr) {
+
+	printf("enter--lte_rece\n");
+
+	int st = socket(AF_INET, SOCK_DGRAM, 0);
+	if (st == -1) {
+		printf("open socket failed ! error message : %s\n", strerror(errno));
+		exit(1);
+	}
+	int port = atoi("5505");
+	 
+	struct sockaddr_in addr;
+	 
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	 
+	if (bind(st, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+		printf("bind IP failed ! error message : %s\n", strerror(errno));
+		exit(1);
+	}
+ 
+	struct sockaddr_in client_addr;
+	socklen_t addrlen = sizeof(client_addr);
+ 
+	int rece_size = 300, k=0;;//修改为随机啊！！！！！！！！！！！
+	uint8_t rece_payload[1000][PAYLOAD_SIZE] ={0};
+	 
+	while (1) {
+
+		if(k == 1000){
+			k = 0;
+		} 
+		 
+		memset(&client_addr, 0, sizeof(client_addr));
+
+		if (recvfrom(st, rece_payload[k], rece_size, 0, (struct sockaddr *)&client_addr, &addrlen) == -1) {
+
+			printf("recvfrom failed ! error message : %s\n", strerror(errno));
+			goto END;
+		}
+		else {
+			//MAC->RLC->IP 第二个参数有误,先固定与接收端一致,但是貌似不影响解包,丢弃了
+			mac_demux_test.process_pdu(rece_payload[k], rece_size);
+			while(!timers_test.get(-1)->is_expired()){ timers_test.get(-1)->step();}		
+		}
+		k++;
+	}
+
+	END:close(st);
+}
---
+++ lte-eNB/enb_ue/test/lte-udp.cc
@@ -0,0 +1,58 @@
+#include "FuncHead.h"
+
+#define SEND_SIZE 400
+
+using namespace srslte;
+using namespace srsue; 
+  
+extern mux ue_mux_test;
+
+void* lte_send_udp(void *ptr) {
+
+	printf("enter--lte_send_udp\n");
+
+	int port = atoi("4404");
+	//create socket
+	int st = socket(AF_INET, SOCK_DGRAM, 0);
+	if (st == -1)
+	{
+		printf("create socket failed ! error message :%s\n", strerror(errno));
+	}
+
+	struct sockaddr_in addr;
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = inet_addr("192.168.3.1");//目的实际地址
+
+	uint8_t *payload_test = new uint8_t[SEND_SIZE];
+	uint8_t *payload_back = new uint8_t[SEND_SIZE];
+
+	uint32_t pdu_sz_test = 300;//下面其实应该发送最终打包长度吧，待修改
+	uint32_t tx_tti_test = 1;
+	uint32_t pid_test = 1;
+
+	while (1) {
+		
+		memset(payload_test, 0, SEND_SIZE*sizeof(uint8_t));
+		memset(payload_back, 0, SEND_SIZE*sizeof(uint8_t));
+
+	     	//uint8_t* mux::pdu_get(uint8_t *payload, uint32_t pdu_sz, uint32_t tx_tti, uint32_t pid)
+		
+		payload_back = ue_mux_test.pdu_get(payload_test, pdu_sz_test, tx_tti_test, pid_test);
+
+
+		if (sendto(st, payload_back, pdu_sz_test, 0, (struct sockaddr *) &addr,
+			sizeof(addr)) == -1)
+		{
+			printf("sendto failed ! error message :%s\n", strerror(errno));
+			break;
+		} 
+		sleep(1);
+	}
+
+	delete[] payload_back;
+	delete[] payload_test;
+
+	close(st);
+}
